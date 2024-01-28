e5211c04262f0ffea15df723f4c84b4b71b95ccf
SheteUC <sheteaa@mail.uc.edu>
2024-01-27 19:24:09 -0500

    scrapping done

--- my_scraper/main.py
+++ my_scraper/main.py
@@ -1,38 +1,33 @@
 from googleapiclient.discovery import build
-from credentials import ggl_api_key, ggl_cse_id
+from credentials import ggl_api_key, ggl_cse_id, DRIVER_PATH
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-# Google Custom Search API
 def google_search(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
     res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
     return res['items']
 
 
-# OpenAI API
 search_results = google_search("Apple About", ggl_api_key, ggl_cse_id, num=3)
 for result in search_results:
     print(result['title'], result['link'])
 
 
-def scrape_url_content(url):
-    try:
-        response = requests.get(url)
+def scrape_dynamic_content(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--headless=new')
+    driver = webdriver.Chrome(options=options)
 
-        # Check if successful
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
+    driver.get(url)
+    driver.implicitly_wait(10)
+    text_content = driver.find_element(By.XPATH, "/html/body").text
+    return text_content
 
-            # Scrape the article body
-            paragraphs = soup.find_all('p')
-            text_content = ' '.join([p.get_text() for p in paragraphs])
-            return text_content
-        else:
-            return "Failed to retrieve content"
-    except Exception as e:
-        return f"An error occurred: {e}"
 
-url = 'https://www.apple.com/business/'
-content = scrape_url_content(url)
-print(content[:500])  # first 500 characters to check
+url = 'https://www.youtube.com/'
+content = scrape_dynamic_content(url)
+print(content)
